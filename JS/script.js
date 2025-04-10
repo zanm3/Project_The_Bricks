897950a2f0adce740ce12f6acc602e118e97804c
Žan Markuža <zan.markuza@tscng.org>
2025-04-10 12:07:28 +0200

    update

--- JS/script.js
+++ JS/script.js
@@ -23,113 +23,123 @@ skrinja.src = "img/treasure_chest.png";
 // točke
 let tocke;
 
-function initbricks() { //inicializacija opek - polnjenje v tabelož
-    bricks = new Array(nrows);
-    for (i = 0; i < nrows; i++) {
-        bricks[i] = new Array(nrows);
-        for (j = 0; j < ncols; j++) {
-            bricks[i][j] = 1;
-        }
+function initbricks() {
+  //inicializacija opek - polnjenje v tabelož
+  bricks = new Array(nrows);
+  for (i = 0; i < nrows; i++) {
+    bricks[i] = new Array(nrows);
+    for (j = 0; j < ncols; j++) {
+      bricks[i][j] = 1;
     }
+  }
 }
 function drawIt() {
-    function init() {
-        canvas = document.getElementById('canvas');
-        ctx = canvas.getContext('2d');
-        init_paddle();
-        initbricks();
-        tocke = 0;
-        $("#tocke").html(tocke);
-        return setInterval(draw, 10); //klic funkcije draw vsakih 10 ms; http://www.w3schools.com/jsref/met_win_setinterval.asp
-    }
-    function init_paddle() {
-        paddlex = canvas.width / 2;
-        paddleh = 10;
-        paddlew = 95;
-    }
-    function onKeyDown(evt) {
-        if (evt.keyCode == 39)
-            rightDown = true;
-        else if (evt.keyCode == 37) leftDown = true;
-    }
+  function init() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    init_paddle();
+    initbricks();
+    tocke = 0;
+    $("#tocke").html(tocke);
+    return setInterval(draw, 10); //klic funkcije draw vsakih 10 ms; http://www.w3schools.com/jsref/met_win_setinterval.asp
+  }
+  function init_paddle() {
+    paddlex = canvas.width / 2;
+    paddleh = 10;
+    paddlew = 95;
+  }
+  function onKeyDown(evt) {
+    if (evt.keyCode == 39) rightDown = true;
+    else if (evt.keyCode == 37) leftDown = true;
+  }
 
-    function onKeyUp(evt) {
-        if (evt.keyCode == 39)
-            rightDown = false;
-        else if (evt.keyCode == 37) leftDown = false;
-    }
+  function onKeyUp(evt) {
+    if (evt.keyCode == 39) rightDown = false;
+    else if (evt.keyCode == 37) leftDown = false;
+  }
 
-    function onSpaceDown(evt){
-        if(evt.keyCode == 32)
-            isCliked = true;
-    }
+  function onSpaceDown(evt) {
+    if (evt.keyCode == 32) isCliked = true;
+  }
 
-    function onSpaceUp(evt){
-        if(evt.keyCode == 32)
-            isCliked = true;
-    }
+  function onSpaceUp(evt) {
+    if (evt.keyCode == 32) isCliked = true;
+  }
 
-    $(document).keydown(onKeyDown);
-    $(document).keyup(onKeyUp);
-    $(document).keydown(onSpaceDown);
-    $(document).keyup(onSpaceUp);
+  $(document).keydown(onKeyDown);
+  $(document).keyup(onKeyUp);
+  $(document).keydown(onSpaceDown);
+  $(document).keyup(onSpaceUp);
 
-    function draw() {
-        // risanje
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.beginPath();
-        ctx.arc(x, y, 7, 0, Math.PI * 2, true)
-        ctx.closePath();
-        ctx.fill();
-        if (rightDown && paddlex + paddlew < canvas.width) paddlex += 5;
-        else if (leftDown && paddlex > 0) paddlex -= 5;
+  function draw() {
+    // risanje
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.arc(x, y, 7, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+    if (rightDown && paddlex + paddlew < canvas.width) paddlex += 5;
+    else if (leftDown && paddlex > 0) paddlex -= 5;
 
-        ctx.fillRect(paddlex, canvas.height - paddleh, paddlew, paddleh);
+    ctx.fillRect(paddlex, canvas.height - paddleh, paddlew, paddleh);
 
-        for (i = 0; i < nrows; i++) {
-            for (j = 0; j < ncols; j++) {
-                if (bricks[i][j] == 1) {
-                    ctx.drawImage(skrinja, (j * (brickwidth + padding)) + padding,
-                        (i * (brickheight + padding)) + padding,
-                        brickwidth, brickheight);
-                }
-            }
+    // izris opek
+    for (i = 0; i < nrows; i++) {
+      for (j = 0; j < ncols; j++) {
+        if (bricks[i][j] == 1) {
+          ctx.drawImage(
+            skrinja,
+            j * (brickwidth + padding) + padding,
+            i * (brickheight + padding) + padding,
+            brickwidth,
+            brickheight
+          );
         }
-
-        rowheight = brickheight + padding; //Smo zadeli opeko?
-        colwidth = brickwidth + padding;
-        row = Math.floor(y / rowheight);
-        col = Math.floor(x / colwidth);
-
-        //Če smo zadeli opeko, vrni povratno kroglo in označi v tabeli, da opeke ni več
-        if (y < nrows * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 1) {
-            dy = -dy; bricks[row][col] = 0;
-            tocke += 1;
-            $("#tocke").html(tocke);
-        }
-
-        if (y + dy > canvas.height - paddleh - 7 && x > paddlex && x < paddlex + paddlew) {
-            dy = -dy; // Reverse the y direction
-            let paddleCenter = paddlex + paddlew / 2;
-            let ballRelativeX = x - paddleCenter;
-            dx = 6 * (ballRelativeX / (paddlew / 2)); // Adjust the x direction based on hit position
-        }
-        if (x + dx > canvas.width - 6 || x + dx < 0 + 6)
-            dx = -dx;
-
-        if (y + dy > canvas.height || y + dy < 0)
-            dy = -dy;
-        else if (y + dy > canvas.height - 6) {
-            if (x > paddlex && x < paddlex + paddlew) {
-                dy = -dy;
-            } else {
-                clearInterval(intervalId);
-            }
-        }
-        x += dx;
-        y += dy;
+      }
     }
-    if(onSpaceDown && onSpaceUp){
-        const intervalId = init();
+
+    rowheight = brickheight + padding; //Smo zadeli opeko?
+    colwidth = brickwidth + padding;
+    row = Math.floor(y / rowheight);
+    col = Math.floor(x / colwidth);
+
+    //Če smo zadeli opeko, vrni povratno kroglo in označi v tabeli, da opeke ni več
+    if (
+      y < nrows * rowheight &&
+      row >= 0 &&
+      col >= 0 &&
+      bricks[row][col] == 1
+    ) {
+      dy = -dy;
+      bricks[row][col] = 0;
+      tocke += 1;
+      $("#tocke").html(tocke);
     }
+
+    if (
+      y + dy > canvas.height - paddleh - 7 &&
+      x > paddlex &&
+      x < paddlex + paddlew
+    ) {
+      dy = -dy; // Reverse the y direction
+      let paddleCenter = paddlex + paddlew / 2;
+      let ballRelativeX = x - paddleCenter;
+      dx = 6 * (ballRelativeX / (paddlew / 2)); // Adjust the x direction based on hit position
+    }
+
+    // premikanje žogice
+    if (x + dx > canvas.width - 6 || x + dx < 0 + 6) dx = -dx;
+
+    if (y + dy > canvas.height || y + dy < 0) dy = -dy;
+    else if (y + dy > canvas.height - 6) {
+      if (x > paddlex && x < paddlex + paddlew) {
+        dy = -dy;
+      } else {
+        clearInterval(intervalId);
+      }
+    }
+    x += dx;
+    y += dy;
+  }
+  const intervalId = init();
 }
